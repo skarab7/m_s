b7d386064e7d1291b85a5cd4fe9803aa478ff2cf
wb <wpjtek@yahoo.com>
2015-04-04 13:40:23 +0200

    Add unit test for ls_lock

--- test/test_ls_lock.py
+++ test/test_ls_lock.py
@@ -3,6 +3,7 @@ import os
 from multiprocessing import Pool
 import fcntl
 import subprocess
+import time
 
 
 class TestLsLock(unittest.TestCase):
@@ -20,19 +21,23 @@ class TestLsLock(unittest.TestCase):
         filename = self._get_lock_file_name("_t1")
         self._create_file_if_not_exist(filename)
         my_pid = os.getpid()
-
         output = None
         with open(filename, "r+") as f:
             fcntl.flock(f.fileno(), fcntl.LOCK_EX)
             output = self._get_output_ls_lock(TestLsLock.TEST_TMP_DIR)
+            # time.sleep(190)       
+ 
         self._delete_file_if_exists(filename)
-        lines = output.split()[2:]
+        lines = output.split("\n")[1:]       
+        is_found = False
+        # TODO: add check for the filename as well
         for l in lines:
-            print l
-            pid = l.split(" ")[0]
-            fl = l.split(" ")[1] 
-            print pid
-            print fl          
+            if l:
+                ls = l.strip()
+                pid = ls.split(" ")[0].strip()
+                if int(pid) == my_pid:
+                    is_found = True
+        self.assertTrue(is_found) 
 
     def _create_file_if_not_exist(self, filename):
         if not os.path.exists(filename):
@@ -47,7 +52,7 @@ class TestLsLock(unittest.TestCase):
         cmd.append(target_dir)
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         output = process.communicate()[0] 
-        return output
+        return output.strip()
 
     def _delete_file_if_exists(self, filename):
         if(os.path.isfile(filename)):
