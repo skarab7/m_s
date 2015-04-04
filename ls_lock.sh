5d99fccb2ffde3933923278f1f3510c470d1671c
wb <wpjtek@yahoo.com>
2015-04-04 11:46:29 +0200

    Complete protype in bash

--- ls_lock.sh
+++ ls_lock.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 set -o errexit
 
+if [ $# -ne 1 ]; then
+  me=`basename $0`
+  echo "No directory name given."
+  echo "Usage $me: my_dicrectory_with_locks"
+  exit 1
+fi
+
+target_dir=$1
+
 index_flock_inodes=()
 
 # credit goes to http://stackoverflow.com/a/14550606/410823
-make_index () {
+make_flock_index () {
   local -a value_array=("$@")
   local i
   for i in "${!value_array[@]}"; do
     eval index_flock_inodes["${value_array[$i]}"]=$i
-    echo "${value_array[$i]}"
   done
 }
 
-
-inode_file_pairs=( $(find m_s -name "*" | xargs -I {} stat --printf="%i,{}\n" {} | sort) )
+inode_file_pairs=( $(find ${target_dir} -name "*" | xargs -I {} stat --printf="%i,{}\n" {} | sort) )
 inode_of_flocks=( $(cat /proc/locks  | grep FLOCK | cut -d' ' -f8 | cut -d':' -f3 | sort) )
 
-make_index "${inode_of_flocks[@]}"
+make_flock_index "${inode_of_flocks[@]}"
+
+printf "%10s %s" "PID" "Lockedfile";
 
 for inode_file in "${inode_file_pairs[@]}"
 do
     inode=${inode_file%,*}  
-    lockfile=${inode_file#*.}
-    inode="12848"  
+    lock_file=${inode_file#*.}
+    # inode="12848"  
     if [ "${index_flock_inodes[$inode]}" ]; then 
-        echo "FOUND" 
+        pid=$(lsof  | grep  ${lock_file}  | cut -d' ' -f2)
+        printf "%10s %s\n" ${pid} ${lock_file}
     fi     
 done
-
-#echo $inode_file_pairs
-#echo $inode_of_flocks
-
-
-
