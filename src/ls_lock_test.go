72137756a9d3a576707d071c944231cbacae2964
wb <wpjtek@yahoo.com>
2015-07-31 12:08:34 +0200

    Simple test passes

--- src/ls_lock_test.go
+++ src/ls_lock_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func Test(t *testing.T) {
+func TestGetInodesInDirectory(t *testing.T) {
 	c, _ := get_inodes_in_directory(".")
 	if len(c.inode_to_file) == 0 {
 		t.Error("Expected to get inodes for the current directory")
