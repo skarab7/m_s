ce0fed5a64d7e21232be642252fbd04c62d3b7d0
wb <wpjtek@yahoo.com>
2015-07-31 12:19:22 +0200

    Make the function to follow golang convention

--- src/ls_lock.go
+++ src/ls_lock.go
@@ -16,18 +16,18 @@ const FIELD_NUM_LC_TYPE int32 = 1
 const FIELD_NUM_INODE = 6
 const FLOCK string = "FLOCK"
 
-func get_inodes_in_directory(target_dir string) (FileInodeCollector, error) {
-	var i FileInodeCollector
-	i.inode_to_file = make(map[uint64]string)
-	err := filepath.Walk(target_dir, i.extract_inode)
-	return i, err
-}
-
 type FileInodeCollector struct {
 	inode_to_file map[uint64]string
 }
 
-func (in *FileInodeCollector) extract_inode(path string, f os.FileInfo, err error) error {
+func GetInodesInDirectory(target_dir string) (FileInodeCollector, error) {
+	var i FileInodeCollector
+	i.inode_to_file = make(map[uint64]string)
+	err := filepath.Walk(target_dir, i.ExtractInode)
+	return i, err
+}
+
+func (in *FileInodeCollector) ExtractInode(path string, f os.FileInfo, err error) error {
 	stat, _ := f.Sys().(*syscall.Stat_t)
 	in.inode_to_file[stat.Ino] = path
 	return nil
@@ -36,7 +36,7 @@ func (in *FileInodeCollector) extract_inode(path string, f os.FileInfo, err erro
 //
 // Get the target directory
 //
-func get_target_directory() (string, int) {
+func GetTargetDirectory() (string, int) {
 	flag.Parse()
 	d := flag.Arg(0)
 	if len(d) == 0 {
@@ -47,10 +47,10 @@ func get_target_directory() (string, int) {
 
 // Complexity
 // O(n)
-func get_indes_used_in_flocks(os_lock_file string) ([]uint64, error) {
+func GetFlocksInodes(os_lock_file string) ([]uint64, error) {
 
 	d, e := ioutil.ReadFile(os_lock_file)
-	check(e)
+	ExitIfError(e)
 	lines := strings.Split(string(d), "\n")
 
 	// ensure that we do not need to reallocate
@@ -76,7 +76,7 @@ func get_indes_used_in_flocks(os_lock_file string) ([]uint64, error) {
 	return lockedInodes, nil
 }
 
-func check(e error) {
+func ExitIfError(e error) {
 	if e != nil {
 		panic(e)
 	}
@@ -84,13 +84,13 @@ func check(e error) {
 
 func main() {
 	var target_dir string
-	target_dir, status := get_target_directory()
+	target_dir, status := GetTargetDirectory()
 	if status != 0 {
 		fmt.Printf("Provide target dir\n")
 		os.Exit(status)
 	}
-	collector, _ := get_inodes_in_directory(target_dir)
-	inodes, _ := get_indes_used_in_flocks("example.txt")
+	collector, _ := GetInodesInDirectory(target_dir)
+	inodes, _ := GetFlocksInodes("example.txt")
 	// merge sort would be the way to go
 
 	for _, i := range inodes {
